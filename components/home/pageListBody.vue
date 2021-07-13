f40cb9388826055923288f620ea4710e8e61586b
2381773226@qq.com <2381773226@qq.com>
2021-07-13 23:16:34 +0800

    update home list style

--- components/home/pageListBody.vue
+++ components/home/pageListBody.vue
@@ -2,7 +2,7 @@
   <div class="pageListBody">
     <ul v-show="list.length">
       <li v-for="(item, index) in list" :key="index">
-        <a :href="'/post/' + item.id" target="_blank" class="post-item">
+        <div class="post-item">
           <img
             :src="imgsList[index]"
             alt=""
@@ -11,23 +11,31 @@
             class="item-img"
           />
           <div class="item-info">
-            <h1 class="home-post-title">{{ item.title }}</h1>
+            <h1 class="home-post-title">
+              <a :href="'/post/' + item.id" target="_blank">{{ item.title }}</a>
+            </h1>
             <div class="home-post-excerpt">
-              {{ deleteHtmlTag(item.content.slice(0, 120)) }} ...
+              <a :href="'/post/' + item.id" target="_blank"
+                >{{ deleteHtmlTag(item.content.slice(0, 120)) }} ...</a
+              >
             </div>
             <div class="home-post-info">
               <span class="create-date el-icon-date">
                 {{ item.createDate }}
               </span>
               <span class="views el-icon-view"> {{ item.views }} </span>
-              <span class="cate-name">{{ item.cateName || "" }} </span>
+              <span class="cate-name"
+                ><a :href="'/post/category/' + item.cate">{{
+                  item.cateName || ""
+                }}</a>
+              </span>
             </div>
           </div>
-        </a>
+        </div>
       </li>
     </ul>
     <div v-show="!list.length">
-        <h2 class="no-data">暂无数据</h2>
+      <h2 class="no-data">暂无数据</h2>
     </div>
   </div>
 </template>
@@ -44,21 +52,20 @@ export default {
   data() {
     return {
       imgsList: [
-        'http://source.dsiab.com/upload/7437e62a-1033-4a91-a357-10993de1a08f.jpg',
-        'http://source.dsiab.com/upload/9a2bae64-f914-4678-a642-95972333e201.jpg',
-        'http://source.dsiab.com/upload/7aaba689-06ff-4621-a1d6-546d86239174.jpg',
-        'http://source.dsiab.com/upload/c0661416-31d9-4b57-9845-802ca01fa69c.jpg',
-        'http://source.dsiab.com/upload/88a60855-9768-4a0b-b202-d4c2e06e6ac8.jpg',
-        'http://source.dsiab.com/upload/08e52e67-b7c7-4de7-8c70-9a9f65d5ce65.jpg',
-        'http://source.dsiab.com/upload/fdf3eba1-5f16-4fdb-91c2-28c22fea7b74.jpg',
-        'http://source.dsiab.com/upload/0913ca3e-fb3c-4120-98fe-62bd86cf9b42.jpeg',
-        'http://source.dsiab.com/upload/5d1c60a0-7194-4c14-b3fe-c7f88d958a78.jpg',
-        'http://source.dsiab.com/upload/cd2a655e-0f6b-41d8-a424-b334dce6cb84.png'
-      ]
-    }
+        "http://source.dsiab.com/upload/7437e62a-1033-4a91-a357-10993de1a08f.jpg",
+        "http://source.dsiab.com/upload/9a2bae64-f914-4678-a642-95972333e201.jpg",
+        "http://source.dsiab.com/upload/7aaba689-06ff-4621-a1d6-546d86239174.jpg",
+        "http://source.dsiab.com/upload/c0661416-31d9-4b57-9845-802ca01fa69c.jpg",
+        "http://source.dsiab.com/upload/88a60855-9768-4a0b-b202-d4c2e06e6ac8.jpg",
+        "http://source.dsiab.com/upload/08e52e67-b7c7-4de7-8c70-9a9f65d5ce65.jpg",
+        "http://source.dsiab.com/upload/fdf3eba1-5f16-4fdb-91c2-28c22fea7b74.jpg",
+        "http://source.dsiab.com/upload/0913ca3e-fb3c-4120-98fe-62bd86cf9b42.jpeg",
+        "http://source.dsiab.com/upload/5d1c60a0-7194-4c14-b3fe-c7f88d958a78.jpg",
+        "http://source.dsiab.com/upload/cd2a655e-0f6b-41d8-a424-b334dce6cb84.png",
+      ],
+    };
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     deleteHtmlTag(str) {
       let str1 = str.replace(/<\/?.+?>/g, "").replace(/&nbsp;/g, "");
@@ -68,24 +75,24 @@ export default {
 };
 </script>
 <style lang="less">
-@media (max-width:575.98px) {
-    .item-img {
-        display: none
-    }
+@media (max-width: 575.98px) {
+  .item-img {
+    display: none;
+  }
 }
 .pageListBody {
-  .no-data{
+  .no-data {
     text-align: center;
   }
   ul {
     li {
       padding: 20px;
       border-bottom: 1px solid #f5f5f5;
-      .home-post-title {
+      .home-post-title a {
         font-size: 20px;
         text-overflow: ellipsis;
       }
-      .home-post-title:hover {
+      .home-post-title a:hover {
         color: #06c;
       }
       .home-post-excerpt {
@@ -102,9 +109,10 @@ export default {
         span {
           margin-right: 10px;
         }
-        .cate-name {
+        .cate-name a {
           font-weight: 400;
           line-height: 18px;
+          color: #828a92;
         }
         .create-date {
           color: #828a92;
@@ -116,7 +124,7 @@ export default {
       .post-item {
         display: flex;
         align-items: center;
-        .item-img{
+        .item-img {
           margin-right: 20px;
         }
       }
