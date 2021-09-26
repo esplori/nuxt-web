c3ec4def6b6df99137579e025e57bbacde3a1443
wilkwo <vincolarick@hotmail.com>
2021-09-26 20:34:25 +0800

    update navigation page

--- pages/navigation/index.vue
+++ pages/navigation/index.vue
@@ -6,16 +6,22 @@
           <div class="left-body">
             <ul>
               <li v-for="(item, index) in list" :key="index">
-                 <div>{{item.cateName}}</div>
-                 <div v-for="(it,ix) in item.children" :key="ix">
-                   <a :href="it.content">{{it.title}}</a>
-                 </div>
+                <div class="cateName">{{ item.cateName }}</div>
+                <div class="child-cate">
+                  <div
+                    class="cate-item"
+                    v-for="(it, ix) in item.children"
+                    :key="ix"
+                  >
+                    <a :href="it.content" target="_blank">{{ it.title }}</a>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
         </el-col>
         <!-- <el-col :span="7"> -->
-          <!-- <sideBar></sideBar> -->
+        <!-- <sideBar></sideBar> -->
         <!-- </el-col> -->
       </el-row>
     </div>
@@ -23,8 +29,7 @@
 </template>
 
 <script>
-
-import {getListApi} from '../api/navigation'
+import { getListApi } from "../api/navigation";
 
 export default {
   head() {
@@ -45,48 +50,58 @@ export default {
     };
   },
   components: {
-    homeHeader: () => import('../../components/homeHeader.vue'),
-    sideBar: () => import('../../components/sideBar.vue'),
-    homeFooter: () => import('../../components/homeFooter.vue')
+    homeHeader: () => import("../../components/homeHeader.vue"),
+    sideBar: () => import("../../components/sideBar.vue"),
+    homeFooter: () => import("../../components/homeFooter.vue"),
   },
   data() {
     return {
-      activeName: 'all'
+      activeName: "all",
     };
   },
-  async asyncData({query,store,$axios}) {
-    let res = await getListApi({})
+  async asyncData({ query, store, $axios }) {
+    let res = await getListApi({});
     return {
-      list: res.data.result
-    }
-  }
+      list: res.data.result,
+    };
+  },
 };
 </script>
 
 <style lang="less" scoped>
 .container {
-  .home-body{
+  .home-body {
     width: 100%;
     margin: 10px auto;
-    .left-body{
+    .left-body {
       background: #fff;
-      box-shadow: 0 1px 3px rgba(27, 95, 160, .1);
-      .nav-tabs{
-        padding: 20px 20px;
-      }
+      box-shadow: 0 1px 3px rgba(27, 95, 160, 0.1);
       ul {
         li {
           padding: 20px;
-          .item-list{
-            display: flex;
+          .cateName {
+            font-size: 20px;
+            padding: 10px 0;
           }
-          a{
-            color: #0474c8;
+          .child-cate {
+            display: flex;
+            .cate-item {
+              font-size: 18px;
+              padding: 10px 10px;
+              border: 1px solid #f5f5f5;
+              border-radius: 5px;
+              margin-right: 10px;
+            }
+            .cate-item:hover{
+              background: #f5f5f5;
+            }
+          }
+          a {
+            color: #666;
           }
         }
       }
     }
   }
 }
-
 </style>
