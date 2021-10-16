730545b3dc0f9a3f3439a3e5b106df01b3bc2771
wilkwo <vincolarick@hotmail.com>
2021-10-16 17:11:06 +0800

    update tongji

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/element-ui',
-    {src: './plugins/bdtongji.js'}
+    {src: './plugins/tongji.js'}
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- plugins/tongji.js
+++ plugins/tongji.js
@@ -2,7 +2,7 @@ var _hmt = _hmt || [];
 (function() {
   if (!process.server) {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?28b51497dbbb25f1b0385bf15b2ffad1";
+    hm.src = "https://s4.cnzz.com/z_stat.php?id=1280444229&web_id=1280444229";
     var s = document.getElementsByTagName("script")[0]; 
     s.parentNode.insertBefore(hm, s);
   }
