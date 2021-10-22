4c4435c34d9dbad45fd628e41fca55e78b8b9675
wilkwo <vincolarick@hotmail.com>
2021-10-22 23:50:42 +0800

    修复接口地址错误

--- pages/api/index.js
+++ pages/api/index.js
@@ -82,7 +82,7 @@ export async function getTagsApi2 (id, data) {
   return get(url, data, options)
 }
 export async function insertCommentApi (data) {
-  const url = baseUrl + '/pages/insertComment'
+  const url =  + '/bootService/pages/insertComment'
   const options = {}
   return post(url, data, options)
 }
