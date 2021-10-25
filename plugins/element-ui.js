16c05370ec90175f718982afc097688884f9b7bf
wilkwo <vincolarick@hotmail.com>
2021-10-25 22:52:37 +0800

    更新element-ui尺寸

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -2,4 +2,4 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 
-Vue.use(Element, { size: 'small' },{ locale })
+Vue.use(Element,{ locale })
