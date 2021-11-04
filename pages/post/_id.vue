fb96374691350bc05439ce6abb950425f1e09a70
wilkwo <vincolarick@hotmail.com>
2021-11-04 22:43:53 +0800

    更新标题

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -41,7 +41,7 @@
       </div> -->
     </div>
 
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="60%">
+    <el-dialog :visible.sync="dialogVisible" width="60%">
       <img :src="imgUrl" alt="" id="bigImg" />
     </el-dialog>
   </div>
