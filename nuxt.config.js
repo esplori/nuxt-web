3f0b5892fdad731258f8f9fd058acccd33156ed0
wilkwo <2381773226@qq.com>
2021-12-23 23:37:06 +0800

    新增Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 'http-equiv': 'content-language', content: 'zh-cn' },
       { hid: 'description', name: 'description', content: 'javascript技术分享,本网站专注开发技术分享,定期分享一些最新的javascript技术，前端教程，后端教程，以及一些资源的分享,致力于书写心得,分享感悟。' },
       { hid: 'keywords', name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
       // { name: 'referrer', content: 'no-referrer-when-downgrade' }, // 解决其他开启网站防盗链功能,全局会导致百度统计失效
