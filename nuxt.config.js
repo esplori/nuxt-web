8761e30b96d9bdf8fb5d2d6df438722db9b35bbf
wilkwo <2381773226@qq.com>
2022-02-10 21:19:27 +0800

    更新网站描述和关键字

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { 'http-equiv': 'content-language', content: 'zh-cn' },
-      { hid: 'description', name: 'description', content: 'javascript技术分享,本网站专注开发技术分享,定期分享一些最新的javascript技术，前端教程，后端教程，以及一些资源的分享,致力于书写心得,分享感悟。' },
-      { hid: 'keywords', name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
+      { hid: 'description', name: 'description', content: '书写心得,分享感悟。' },
+      { hid: 'keywords', name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端,前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
       // { name: 'referrer', content: 'no-referrer-when-downgrade' }, // 解决其他开启网站防盗链功能,全局会导致百度统计失效
 
     ],
@@ -54,8 +54,8 @@ export default {
   },
   proxy: {
     '/bootService': {
-      // target: 'http://localhost:8091/'
-      target: 'http://admin.dsiab.com/'
+      target: 'http://localhost:8091/'
+      // target: 'http://admin.dsiab.com/'
     }
   },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
