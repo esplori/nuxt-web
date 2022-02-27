0c42d4fea10af4bd55c73005378fecabcff508ff
wilkwo <2381773226@qq.com>
2022-02-27 19:27:52 +0800

    更新统计插件逻辑

--- components/common/homeFooter.vue
+++ components/common/homeFooter.vue
@@ -15,8 +15,8 @@ export default {
     return {
       script: [
         { src: "https://cpro.baidustatic.com/cpro/ui/cm.js" },
-        { src: "https://source.dsiab.com/compress-js/dist/js/WebStats.js" },
         { src: "https://source.dsiab.com/compress-js/dist/js/fp.min.js" },
+        { src: "https://source.dsiab.com/compress-js/dist/js/WebStats.js" },
       ],
     };
   },
@@ -24,28 +24,21 @@ export default {
     if (process.client) {
       // 添加统计
       this.initFingerprint();
-      // this.initWebStat();
-      window.addEventListener("click", (item) => {
-        // this.getWegStats();
-      });
     }
   },
   methods: {
-    getWegStats() {
-      window.webStats.track("/bootService/stats/getStats.gif", {
-        ...window.webStats.getDirectData(),
-        ...{ visitorId: window.visitorId || "" },
-      });
-    },
-    initWebStat() {
+    initWebStat(visitorId) {
       let webStats = new WebStats({
         baseUrl: "/bootService", // 基础接口地址url
         url: "/stats/getStats.gif", // 请求上报api的接口地址
         routeMode: "history", // 填写单页面应用中使用的路由模式。
         autoUpload: true, // 是否自动请求接口，在setUserId之后会以baseUrl+url形式在页面切换时自动请求上报PV/UV的接口
+        prop: {
+          //请求参数映射，参数名默认如下，可以自定义修改参数名。
+          id: "visitorId",
+        },
       });
-      window.webStats = webStats;
-      this.getWegStats();
+      webStats.setUserId(visitorId);
     },
     initFingerprint() {
       let _this = this;
@@ -59,9 +52,8 @@ export default {
         // This is the visitor identifier:
         const visitorId = result.visitorId;
         // console.log(visitorId);
-        window.visitorId = visitorId;
         setTimeout(() => {
-          _this.initWebStat();
+          _this.initWebStat(visitorId);
         }, 200);
       })();
     },
