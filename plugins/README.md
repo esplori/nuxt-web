d70339701663a549e5085717690b1c3ead43713b
wilkwo <2381773226@qq.com>
2022-03-04 15:26:23 +0800

    删除无效文件

--- plugins/README.md
+++
@@ -1,8 +0,0 @@
-# 目录说明
- axios.js: 封装axios
-
- baseUrl.js: 后端接口地址
-
- btongji.js: 百度广告js
- 
- element-ui.js：element-ui 文件
--- plugins/utils.js
+++ plugins/utils.js
@@ -1,3 +1,8 @@
+/**
+ * 删除html标签
+ * @param {*} str 
+ * @returns 
+ */
 export function delHtmlTag(str) {
   let str1 = str.replace(/<\/?.+?>/g, "").replace(/&nbsp;/g, "").replace(/&lt;/g, "").replace(/&gt;/g, "");
   return str1.length > 120 ? str1.slice(0,120) + '...' : str1;
