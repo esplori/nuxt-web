1c8fc07c4eb8c7b2bc586de48ca50d8844272bcf
wilkwo <2381773226@qq.com>
2022-05-17 23:34:41 +0800

    删除ument统计

--- plugins/tongji.js
+++ plugins/tongji.js
@@ -1,9 +1,8 @@
-var _hmt = _hmt || [];
 (function () {
   if (process.client) {
-    var hm = document.createElement("script");
-    hm.src = "https://s4.cnzz.com/z_stat.php?id=1280444229&web_id=1280444229";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
+    // var hm = document.createElement("script");
+    // hm.src = "https://s4.cnzz.com/z_stat.php?id=1280444229&web_id=1280444229";
+    // var s = document.getElementsByTagName("script")[0];
+    // s.parentNode.insertBefore(hm, s);
   }
 })();
