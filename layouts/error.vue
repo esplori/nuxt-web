dee63c660907b00cfbe17f16271b04ea72acdc15
wilkwo <2381773226@qq.com>
2022-10-16 21:05:49 +0800

    更新404页面

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <img src="@/assets/img/404.png" alt="" width="100%" />
     <div class="msg">抱歉，你访问的页面不存在</div>
-    <div class="seconds">{{ seconds }} 秒后将跳转改成首页</div>
+    <div class="seconds">{{ seconds }} 秒后将跳转到首页</div>
   </div>
 </template>
 <script>
@@ -17,7 +17,8 @@ export default {
     if (process.client) {
       setInterval(() => {
         this.seconds = this.seconds - 1;
-        if (this.seconds >= 0) {
+        if (this.seconds <= 0) {
+          this.seconds = 0
           location.href = "/";
         }
       }, 1000);
@@ -30,6 +31,7 @@ export default {
   background: #fff;
   height: 100%;
   padding-bottom: 200px;
+
   .msg,
   .seconds {
     text-align: center;
