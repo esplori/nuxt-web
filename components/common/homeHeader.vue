8cd7b001ccf92b3670911501a2b4af4b71931b2d
wilkwo <2381773226@qq.com>
2022-10-22 20:21:14 +0800

    新增登录入口

--- components/common/homeHeader.vue
+++ components/common/homeHeader.vue
@@ -36,6 +36,11 @@
               >更新日志</a
             >
           </el-menu-item>
+          <el-menu-item>
+            <a href="http://admin.dsiab.com/#/login" target="_blank"
+              >登录</a
+            >
+          </el-menu-item>
         </el-menu>
       </div>
     </div>
