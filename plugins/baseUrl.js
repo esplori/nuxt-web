a4900e5c2d4cecebfd594d8d2c9b4b9b7d3369c4
2EN <2381773226@QQ.COM>
2023-03-15 21:46:39 +0800

    更新接口地址

--- plugins/baseUrl.js
+++ plugins/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = 'http://admin.dsiab.com/bootService'
+const baseUrl = 'http://admin.dsiab.com/portal-service'
 // const baseUrl = 'http://localhost:8091/bootService'
 export default baseUrl
